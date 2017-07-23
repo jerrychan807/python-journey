7fb81088665758615994918c8dea1ccaae0ed63e
jerrychan807 <529883409@qq.com>
2017-07-23 18:27:30 +0800

    .
    .

---
+++ return Network_Interface_Card result/README.md
@@ -0,0 +1,12 @@
+#return_centos_eth_config.py
+- 返回centos下系统的网卡的配置信息，以json的格式
+- ![](https://ws2.sinaimg.cn/large/006tNc79gy1fhtzqvi4noj31kw0bc7a1.jpg)
+- [实现思路说明](https://jerrychan807.gitbooks.io/my-python-cookbook/content/huo-qu-centos-xia-de-suo-you-wang-qia-de-pei-zhi-xin-xi.html)
+
+
+
+#return_wins_eth_config.py
+ - 返回windows server 2008 下的网卡配置信息，以json的格式 	
+
+
+
--- return Network_Interface_Card result/return_centos_eth_config.py
+++ return Network_Interface_Card result/return_centos_eth_config.py
@@ -12,8 +12,10 @@ import commands
 
 '''
 
+#获取所有dns配置信息,并返回json格式的结果
 def return_dns_list_json_result():
-    # 初始化一个字典,存放dns数据信息的容器
+
+    # 初始化一个字典,作为存放dns数据信息的容器
     dns_list = {}
 
     dns_output = os.popen("cat /etc/resolv.conf")
@@ -34,16 +36,15 @@ def return_dns_list_json_result():
 
 
 
-
+#获取所有网卡配置信息,并返回json格式的结果
 def return_eth_config_json_result():
-    list=[]
     json_dictlist=[]
     json_dict=''
 
     #初始化装载 json格式的配置信息容器
     eth_config_list_json_result = ''
 
-    #定义需要在配置文件中匹配的参数列表
+    #定义待查找的参数列表
     parameter_list=['DEVICE','IPADDR','NETMASK','GATEWAY']
 
     #初始化一个字典,作为存放网卡配置的信息的容器
@@ -56,27 +57,37 @@ def return_eth_config_json_result():
     (status,allfilename) = commands.getstatusoutput("ls /etc/sysconfig/network-scripts/ | grep '^ifcfg.*[0-9.*]$'")
 
 
-
-
     #以换行符为分割,将所有文件名混合在一起的字符串分割为几个单独文件名
     #filename = ['ifcfg-eth0','ifcfg-eth1','ifcfg-eth2']
     filename = allfilename.split("\n")
 
+    # 初始化数值
+    parameter_list = ['DEVICE', 'IPADDR', 'NETMASK', 'GATEWAY']
+    for parameter in parameter_list:
+        eth_infomatoin_dict[parameter] = '0'
+        
 
     for eachfile in filename: #遍历配置文件名
         eth_infomatoin_output = os.popen('cat /etc/sysconfig/network-scripts/'+ eachfile) #输出对应文件名内的配置信息
-        for line in eth_infomatoin_output: #遍历每行配置项
-            for parameter in parameter_list: #如果含有某些参数就继续执行
-                if parameter in line:
+        for line in eth_infomatoin_output: #遍历配置文件的每一行
+            for parameter in parameter_list: #遍历待查找参数列表
+                if parameter in line: #如果在某行含有待查找参数列表中的列表
+                    parameter_list.remove(parameter) #如果找到该参数,则从需要待查找的参数列表中删去
                     temp = line.strip('\n').split('=') #去掉末尾换行符,以等号分割
-                    #json_dict[parameter] = temp[0]
                     value =  temp[-1] # 取等号右边的数值信息
                     eth_infomatoin_dict[parameter] =value.strip("\"")
+
 
         #将每个网卡的配置信息转换成json格式
         jsontemp = json.dumps(eth_infomatoin_dict)
         json_dict = "\""+"%s\":"%eachfile+jsontemp
         json_dictlist.append(json_dict)
+
+        # 初始化数值
+        parameter_list = ['DEVICE', 'IPADDR', 'NETMASK', 'GATEWAY']
+        for parameter in parameter_list:
+            eth_infomatoin_dict[parameter] = '0'
+
 
     for i in range(0,len(json_dictlist)):
         tmp =  json_dictlist[i]+ ','
@@ -84,7 +95,7 @@ def return_eth_config_json_result():
 
     return eth_config_list_json_result
 
-
+#输出json格式化的最终结果
 def output_result(eth_result,dns_result):
     result = ''
     result = eth_result + dns_result
@@ -95,6 +106,7 @@ def output_result(eth_result,dns_result):
 
 
 if __name__ == '__main__':
+
     eth_config_json_result = return_eth_config_json_result()
     dns_list_json_result = return_dns_list_json_result()
 
